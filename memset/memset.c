3d587c81278a19ff6efe9108847e668d6305cad4
CursedIndividual <mehceu@gmail.com>
2022-03-04 00:44:03 +0300

    bugfix for memset

--- memset/memset.c
+++ memset/memset.c
@@ -2,9 +2,6 @@
 
 void mem_set(const void* src, int set_val, const unsigned int start_index, const unsigned int last_index) {
     
-    if (start_index > start_index)
-        return;
-    
     for (int i = start_index; i < last_index; i++) {
         ((char*)src)[i] = set_val;
     }
